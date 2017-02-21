301832c660ad5add520a90b780e4ac56b4fc3a1f
vncnliu <vncnliu@gmail.com>
2017-02-21 18:14:10 +0800

    default

--- src/me/vncnliu/attack/clojure/self/self.clj
+++ src/me/vncnliu/attack/clojure/self/self.clj
@@ -30,3 +30,22 @@
     (if (= partln (length part-array))
       part-array
       (rand-part-array array part-array partln (dec swapln)))))
+
+(defn rand_part [array partln part-array]
+  (if (= (count part-array) (+ partln (count part-array)) )
+    part-array
+    (if (< (rand) (/ partln (count array)))
+      (rand_part (rest array) (dec partln) (conj part-array (first array)))
+      (rand_part (rest array) partln part-array))))
+
+(use 'clojure.java.io)
+
+(defn get_str [n]
+  (if (< n 1)
+    ()
+    (conj (get_str (dec n)) (rand_part (list 1 2 3 4 5 6) 3 ()))))
+
+(dotimes [n 10000] (with-open [wrtr (writer "a.txt":append true)]
+                     (.write wrtr (prn-str (rand_part (list 1 2 3 4) 3 ())))))
+
+;(dotimes [n 5] (println (rand_part (list 1 2 3 4 5 6) 3 ())))
